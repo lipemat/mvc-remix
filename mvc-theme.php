5432f78c110f18ddb030eaeffb44a35fd3dcd12b
Mat Lipe <mat@lipeimagination.info>
2013-11-12 12:30:50 -0600

    Update mvc-theme.php
    version 0.4.0 ready to come out and play

--- mvc-theme.php
+++ mvc-theme.php
@@ -4,7 +4,7 @@ Plugin Name: Mvc Theme
 Plugin URI: http://matlipe.com 
 Description: Turns any Genesis theme into a Model View Controller driven framework.
 Author: Mat Lipe
-Version: 0.3.1
+Version: 0.4.0
 Author URI: http://matlipe.com
 */
 
