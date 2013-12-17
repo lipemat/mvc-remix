62b1b7fc64d1e2af2534551d4c0ff5caa1af01fc
lipemat <mat@matlipe.com>
2013-12-16 18:02:47 -0600

    turn off category icons by default

--- mvc-config.php
+++ mvc-config.php
@@ -40,6 +40,6 @@ add_theme_support( 'mvc_update' );
 
 //add_theme_support('mvc_mobile_responsive');
 //add_theme_support('mvc_mobile_menu','dark'); //light or dark
-add_theme_support('mvc_category_images');
+//add_theme_support('mvc_category_images');
 //add_theme_support('mvc_image_resize');
 
