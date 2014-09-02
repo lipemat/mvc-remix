cefac6af3378837003eba99fb70f24612aa2db0c
lipemat <mat@matlipe.com>
2014-09-02 17:21:29 -0500

    move network settings to site options instead of site 1 options

--- classes/MVC/Settings.php
+++ classes/MVC/Settings.php
@@ -5,151 +5,153 @@ namespace MVC;
  * Settings
  *
  * Abstract starting point for a settings page
+ * Retrieve option from proper location by using $this->get_option()
  *
  *
- * @uses extend this with another class that does not have a __construct method or call parent::__construct()
+ * @uses      extend this with another class that does not have a __construct method or call parent::__construct()
  *       Implement the abstract methods and set appropriate class vars. This will do the rest.
  *
- * @uses to have a description for a section create a public method %section_slug%_description and
+ * @uses      to have a description for a section create a public method %section_slug%_description and
  *       it will automatically be used
  *
- * @uses to override the default text field create a protected method with same name as option and
+ * @uses      to override the default text field create a protected method with same name as option and
  *       it will be passed the value of the option as it only argument
  *
- * @package MVC Theme
+ *
+ * @package   MVC Theme
  * @namespace MVC
  *
- * @class Settings
+ * @class     Settings
  *
  */
 abstract class Settings {
 
 	/**
 	 * Title
-	 * 
+	 *
 	 * Menu and Settings page title
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $title;
-	
+
 	/**
 	 * Menu Title
-	 * 
+	 *
 	 * Menu item label ( defaults to $this->title )
-	 * 
+	 *
 	 * @var string
 	 */
-	protected $menu_title; 
-	
+	protected $menu_title;
+
 	/**
 	 * Slug
-	 * 
+	 *
 	 * Settings slug
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $slug;
-	
+
 	/**
 	 * Parent menu Slug
-	 * 
+	 *
 	 * Where should we put this menu
-	 * 
+	 *
 	 * @uses leave blank for a top level menu
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $parent_menu_slug = 'options-general.php';
-	
+
 	/**
 	 * Menu Icon
-	 * 
+	 *
 	 * If you are creating a main level menu use an icon
-	 * 
+	 *
 	 * @var string Url
 	 */
 	protected $menu_icon;
-	
+
 	/**
 	 * Menu Position
-	 * 
+	 *
 	 * If you would like specify a menu order
-	 * 
+	 *
 	 * @var int
 	 */
-	 protected $menu_position;
-	
+	protected $menu_position;
+
 	/**
 	 * Capability
-	 * 
+	 *
 	 * What permission do I need to use this menu
-	 * 
+	 *
 	 * @var string
 	 */
 	protected $capability = 'manage_options';
-	
+
 	/**
 	 * Network
-	 * 
+	 *
 	 * Network admin menu?
-	 * 
+	 *
 	 * @var bool
 	 */
 	protected $network = false;
-	
-	
+
+
 	/**
 	 * Settings
-	 * 
+	 *
 	 * Set me within your add_settings() method
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $settings = array();
-	
-	
+
 
 	/**
 	 * Add Settings
-	 * 
+	 *
 	 * Method used to set the settings
 	 * Separate from set_vars to keep things cleaner
-	 * 
-	 * @uses $this->settings
-	 * 
+	 *
+	 * @uses    $this->settings
+	 *
 	 * @example $this->settings = array(
-	 * 		'career-page' => array(
-	 * 			'title'  	=> 'Career Page',
-	 *          'fields' 	=> array(
-	 * 				'career_heading_message' => 'Heading Message'
-	 * 				
-	 * 			)
-	 * 		)
+	 *        'career-page' => array(
+	 *            'title'    => 'Career Page',
+	 *          'fields'    => array(
+	 *                'career_heading_message' => 'Heading Message'
+	 *
+	 *            )
+	 *        )
 	 * );
-	 * 
+	 *
 	 */
 	abstract protected function add_settings();
-	
+
+
 	/**
 	 * Set Vars
-	 * 
+	 *
 	 * Use this method to set the necessary class vars
-	 * 
+	 *
 	 * @see This classes vars
-	 * 
+	 *
 	 * @return void
-	 * 
+	 *
 	 */
 	abstract protected function set_vars();
 
-	
+
 	/**
 	 * Call
-	 * 
+	 *
 	 */
 	function __call( $name, $args ){
-				
+
 	}
 
 
@@ -157,85 +159,110 @@ abstract class Settings {
 	 * Construct
 	 *
 	 */
-	function __construct() {	
+	function __construct(){
 		$this->add_settings();
 		$this->set_vars();
 		$this->fill_class_vars();
 		$this->hooks();
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Hook em up
-	 * 
+	 *
 	 * @return void
 	 */
 	private function hooks(){
 		if( $this->network ){
-			add_action( 'network_admin_menu', array( $this, 'register_settings_page' ), 10, 0 );	
-			add_action( 'admin_menu', array( $this, 'register_settings' ) );	
-		} else {		
+			add_action( 'network_admin_menu', array( $this, 'register_settings_page' ), 10, 0 );
+			add_action( 'network_admin_edit_' . $this->slug, array( $this, 'save_network_settings' ), 10, 0 );
+
+		} else {
 			add_action( 'admin_menu', array( $this, 'register_settings_page' ), 10, 0 );
-			
+
 		}
-		
+
 	}
 
 
 	/**
-	 * Add Options
-	 * 
-	 * Used to register settings for use with Network admin
-	 * The network_admin_menu will not register the settings so you get a no options page error
-	 * 
-	 * @uses called via admin_menu when using the $this->network
-	 * 
+	 * Get Option
+	 *
+	 * Get a site option or regular depending if we are network or not
+	 *
+	 * @param string $field
+	 *
+	 * @return mixed|void
+	 */
+	public function get_option( $field ){
+		if( $this->network ){
+			return get_site_option( $field, null );
+		} else {
+			return get_option( $field, null );
+		}
+	}
+
+
+	/**
+	 * Save Network Settings
+	 *
+	 * Saves the settings if on a network page
+	 * Uses update_site_option() instead of update_site_option
+	 *
 	 * @return void
 	 */
-	public function register_settings(){
+	public function save_network_settings(){
+		if( !isset( $_POST[ '_wpnonce' ] ) || !wp_verify_nonce( $_POST[ '_wpnonce' ], $this->slug . '-options' ) ){
+			return;
+		}
+
 		foreach( $this->settings as $section => $params ){
 			foreach( $params[ 'fields' ] as $field => $title ){
-				register_setting( $this->slug, $field );
+				update_site_option( $field, $_POST[ $field ] );
 			}
-		}	
-		
+		}
+
+		wp_redirect( add_query_arg( array( 'page' => $this->slug, 'updated' => 'true' ), network_admin_url( $this->parent_menu_slug ) ) );
+
+		exit();
+
 	}
-	
-	
+
+
 	/**
 	 * Register Settings Page
-	 * 
+	 *
 	 * Build the settings page using the options framework
-	 * 
+	 *
 	 * @uses $this->settings
-	 * 
+	 *
 	 * @return void
-	 * 
+	 *
 	 */
 	public function register_settings_page(){
-		
+
 		if( !empty( $this->parent_menu_slug ) ){
 			add_submenu_page(
-				$this->parent_menu_slug, 
+				$this->parent_menu_slug,
 				$this->title,
-				$this->menu_title, 
-				$this->capability, 
+				$this->menu_title,
+				$this->capability,
 				$this->slug,
 				array( $this, 'display_settings_page' )
 			);
 		} else {
 			add_menu_page(
 				$this->title,
-				$this->menu_title, 
-				$this->capability, 
+				$this->menu_title,
+				$this->capability,
 				$this->slug,
 				array( $this, 'display_settings_page' ),
 				$this->menu_icon,
 				$this->menu_position
 			);
 		}
-		
+
 		foreach( $this->settings as $section => $params ){
 			add_settings_section(
 				$section,
@@ -243,7 +270,7 @@ abstract class Settings {
 				array( $this, $section . '_description' ),
 				$this->slug
 			);
-			
+
 			foreach( $params[ 'fields' ] as $field => $title ){
 				add_settings_field(
 					$field,
@@ -253,64 +280,74 @@ abstract class Settings {
 					$section,
 					$field
 				);
-				
-				register_setting( $this->slug, $field );
+
+				if( !$this->network ){
+					register_setting( $this->slug, $field );
+				}
 			}
 		}
-	
+
 	}
-	
-	
+
+
 	/**
 	 * Field
-	 * 
+	 *
 	 * Will call a method matching the field name if exists
 	 * Otherwise outputs a standard text field
-	 * 
-	 * @param array $item
+	 *
+	 * @param array  $item
 	 * @param string $field
-	 * 
+	 *
 	 * @return void
-	 * 
+	 *
 	 */
 	public function field( $field ){
-		
-		$value = get_option( $field, '' );
-		
+
+		if( $this->network ){
+			$value = get_site_option( $field, '' );
+		} else {
+			$value = get_option( $field, '' );
+		}
+
 		if( method_exists( $this, $field ) ){
-			$this->{$field}( $value );	
+			$this->{$field}( $value );
+
 			return;
 		}
-		
+
 		printf( '<input name="%1$s" value="%2$s" />', $field, $value );
-		
+
 	}
-	
-	
+
 
 	/**
 	 * Fill Class Vars
-	 * 
+	 *
 	 * Did you forget something? Oh well, this will fix it
-	 * 
+	 *
 	 * @return void
 	 */
 	private function fill_class_vars(){
-		
+
 		if( empty( $this->title ) ){
-			$this->title = __( 'Settings', 'mvc' );	
-		}	
-		
+			$this->title = __( 'Settings', 'mvc' );
+		}
+
 		if( empty( $this->slug ) ){
 			$this->slug = strtolower( str_replace( '\\', '-', get_class( $this ) ) );
 		}
-		
+
 		if( empty( $this->menu_title ) ){
-			$this->menu_title = $this->title;	
+			$this->menu_title = $this->title;
+		}
+
+		if( $this->network ){
+			if( 'options-general.php' == $this->parent_menu_slug ){
+				$this->parent_menu_slug = 'settings.php';
+			}
 		}
 	}
-	
-	
 
 
 	/**
@@ -320,11 +357,18 @@ abstract class Settings {
 	 *
 	 * @return void
 	 */
-	public function display_settings_page() {
+	public function display_settings_page(){
+		if( $this->network ){
+			$url = network_admin_url( 'edit.php?action=' . $this->slug );
+		} else {
+			$url = admin_url( 'options.php' );
+		}
+
 		?>
 		<div class="wrap">
 			<h2><?php echo $this->title; ?></h2>
-			<form action="<?php echo admin_url( 'options.php' ); ?>" method="post">
+
+			<form action="<?php echo $url; ?>" method="post">
 				<?php
 				settings_fields( $this->slug );
 				do_settings_sections( $this->slug );
@@ -332,8 +376,8 @@ abstract class Settings {
 				?>
 			</form>
 		</div>
-		<?php
-		
+	<?php
+
 	}
 
 }
