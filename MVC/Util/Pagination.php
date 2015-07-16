9efd54d6619fb8f5666050ad59546b607547d6ca
lipemat <mat@matlipe.com>
2015-07-16 10:11:35 -0400

    Improve pagination to allow for submitted a wp_query

--- MVC/Util/Pagination.php
+++ MVC/Util/Pagination.php
@@ -16,24 +16,55 @@ namespace MVC\Util;
  */
 class Pagination {
 
-	const PER_PAGE = 8;
+	private $per_page = 10;
 
 	private $items;
 
+	/**
+	 * wp_query
+	 *
+	 * @var \WP_Query|null
+	 */
+	private $wp_query = null;
+
 	private $page;
 
-	public function __construct( $items, $current_page ){
-		$this->items = $items;
-		$this->page = $current_page;
+
+	/**
+	 * Send me some items and i'll create the pagination.
+	 * If no wp_query is used I will split the list and return the items within that range
+	 * If a wp_query is used, I will return the same items you sent me
+	 *
+	 * @param array $items              - either the full list of items, or this page's posts when using a wp_query
+	 * @param \WP_Query|false [$wp_query] - send a wp_query to use that for handling calculations instead of an
+	 *                                  independent list
+	 * @param       int                 [$per_page] - defaults to 10
+	 */
+	public function __construct( $items, \WP_Query $wp_query = null, $per_page = 10 ){
+		$this->items    = $items;
+		$this->wp_query = $wp_query;
+		$this->per_page = 8;
+
+		if( $wp_query ){
+			$page = $this->wp_query->is_paged() ? $wp_query->query_vars[ 'paged' ] : 1;
+		} else {
+			$page = empty( $_REQUEST[ 'page' ] ) ? 1 : $_REQUEST[ 'page' ];
+		}
+		$this->page = $page;
 	}
 
+
 	public function get_total_pages(){
-		$count = count( $this->items );
+		if( $this->wp_query ){
+			$count = $this->wp_query->found_posts;
+		} else {
+			$count = count( $this->items );
+		}
 		if( $count == 0 ){
 			return 0;
 		}
 
-		return ceil( $count / self::PER_PAGE );
+		return ceil( $count / $this->per_page );
 	}
 
 
@@ -44,10 +75,14 @@ class Pagination {
 	 * @return array
 	 */
 	public function get_this_pages_items(){
+		//wp_query already gave us this page's items in the first place
+		if( $this->wp_query ){
+			return $this->items;
+		}
 		$page   = $this->page;
-		$bottom = ( $page - 1 ) * self::PER_PAGE;
+		$bottom = ( $page - 1 ) * $this->per_page;
 
-		$items = array_slice( $this->items, $bottom, self::PER_PAGE );
+		$items = array_slice( $this->items, $bottom, $this->per_page );
 
 		return $items;
 	}
@@ -71,7 +106,7 @@ class Pagination {
 
 		if( $page < 3 ){
 			$bottom = 1;
-		} elseif( $page == $total ){
+		} elseif( $page == $total ) {
 			$bottom = max( 1, $page - 5 );
 		} elseif( $page + 3 > $total ) {
 			$bottom = max( 1, $page - 4 );
@@ -85,8 +120,107 @@ class Pagination {
 			$top = $bottom + 4;
 		}
 
+		if( $this->wp_query ){
+			$this->link_html( $page, $total, $top, $bottom );
+		} else {
+			$this->ajax_html( $page, $total, $top, $bottom );
+		}
+	}
+
+
+	/**
+	 * Generates the html based on links /page/%number%
+	 * Used by standard wp queries.
+	 *
+	 * Used when we do have a wp_query
+	 *
+	 * @param $page
+	 * @param $total
+	 * @param $top
+	 * @param $bottom
+	 *
+	 * @return void
+	 */
+	private function link_html( $page, $total, $top, $bottom ){
+
+		get_next_posts_link()
 		?>
-		<ul class="mvc-pagination">
+		<ul class="navigation">
+			<?php
+			if( $page != "1" ){
+				?>
+				<li>
+					<a href="<?php echo get_pagenum_link( 1 ); ?>">
+						&laquo;
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo get_pagenum_link( $page - 1 ); ?>">
+						&lt;
+					</a>
+				</li>
+				<?php
+			}
+
+			while( $bottom <= $top ){
+				$class = '';
+				if( $bottom == $page ){
+					$class = ' class="current"';
+				}
+				?>
+				<li>
+					<a href="<?php echo get_pagenum_link( $bottom ); ?>"<?php echo $class; ?>>
+						<?php _e( $bottom, 'edspire' ); ?>
+					</a>
+				</li>
+				<?php
+				$bottom ++;
+			}
+
+			?>
+			<li>
+				...
+			</li>
+			<li>
+				<a href="<?php echo get_pagenum_link( $total ); ?>">
+					<?php _e( $total, 'edspire' ); ?>
+				</a>
+			</li>
+			<?php
+			if( $page != $total ){
+				?>
+				<li>
+					<a href="<?php echo get_pagenum_link( $page + 1 ); ?>">
+						&gt;
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo get_pagenum_link( $total ); ?>">
+						&raquo;
+					</a>
+				</li>
+				<?php
+			}
+			?>
+		</ul>
+		<?php
+	}
+
+
+	/**
+	 * Generate the ajax driven pagination
+	 * Used when we don't have a wp_query
+	 *
+	 * @param $page
+	 * @param $total
+	 * @param $top
+	 * @param $bottom
+	 *
+	 * @return void
+	 */
+	private function ajax_html( $page, $total, $top, $bottom ){
+		?>
+		<ul class="cc-pagination navigation">
 			<?php
 			if( $page != "1" ){
 				?>
@@ -100,7 +234,7 @@ class Pagination {
 						&lt;
 					</a>
 				</li>
-			<?php
+				<?php
 			}
 
 			while( $bottom <= $top ){
@@ -140,10 +274,10 @@ class Pagination {
 						&raquo;
 					</a>
 				</li>
-			<?php
+				<?php
 			}
 			?>
 		</ul>
-	<?php
+		<?php
 	}
 }
