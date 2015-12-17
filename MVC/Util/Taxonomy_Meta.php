6f3fe846994428b236e79ca2ab82a61c18f5a1dd
lipemat <mat@matlipe.com>
2015-12-17 10:43:17 -0500

    Migrate legacy term meta over to new WP core structure and deprecate usage of current class

--- MVC/Util/Taxonomy_Meta.php
+++ MVC/Util/Taxonomy_Meta.php
@@ -5,47 +5,125 @@ namespace MVC\Util;
 /**
  * Taxonomy_Meta
  *
- * @author  Mat Lipe
- * @since   6/10/2015
- *
- * @package MVC\Util
+ * @deprecated as of version 4.4 of WordPress
  */
 class Taxonomy_Meta {
-
 	use \MVC\Traits\Singleton;
 
 	const DB_OPTION = 'mvc_taxonomy_meta';
 
-
 	public function hooks(){
-		add_action( 'init', array( $this, 'configure_term_meta' ), 0, 0 );
-		add_action( 'created_term', array( $this, 'delete_extra_meta' ), - 1000, 3 );
-		add_action( 'delete_term', array( $this, 'delete_all_term_meta' ), 10, 3 );
+
+		//less than version 4.4
+		if( version_compare( $GLOBALS[ 'wp_version' ], "4.4" ) === - 1 ){
+			add_action( 'init', array( $this, 'configure_term_meta' ), 0, 0 );
+			add_action( 'created_term', array( $this, 'delete_extra_meta' ), - 1000, 3 );
+			add_action( 'delete_term', array( $this, 'delete_all_term_meta' ), 10, 3 );
+			add_action( 'switch_blog', array( $this, 'switch_to_blog' ), 10, 2 );
+		}
+
+		add_action( 'wp_upgrade', array( $this, 'migrate_data_to_core_structure' ), 1, 2 );
 		add_action( 'wpmu_drop_tables', array( $this, 'drop_table' ), 10, 1 );
-		add_action( 'switch_blog', array( $this, 'switch_to_blog' ), 10, 2 );
+
 	}
 
 
+	/**
+	 * Version 4.4 implemented their own term meta structure.
+	 * The terms used to be able to share term_ids and only
+	 * had a unique term_taxonomy_id. That is not the case any
+	 * more so this migrates all the old data over to the new
+	 * termmeta table.
+	 *
+	 *
+	 * @param int $db_version            - upgrading to version
+	 * @param int $wp_current_db_version - current (old) version
+	 *
+	 * @return void
+	 */
+	public function migrate_data_to_core_structure( $db_version, $wp_current_db_version ){
+		//4.4 or greater coming from 4.3 or lower
+		if( $db_version >= 35700 && $wp_current_db_version < 35700 ){
+			global $wpdb;
+			$query = "SELECT term_id, meta_key, meta_value
+						FROM $wpdb->term_taxonomymeta AS meta
+						LEFT JOIN $wpdb->term_taxonomy AS terms
+						USING (term_taxonomy_id)";
+			$meta  = $wpdb->get_results( $query );
+
+			$import_query  = "INSERT INTO $wpdb->termmeta (term_id, meta_key, meta_value)";
+			$sql_query_sel = array();
+			foreach( $meta as $_meta ){
+				$meta_key        = $_meta->meta_key;
+				$meta_value      = addslashes( $_meta->meta_value );
+				$sql_query_sel[] = "SELECT $_meta->term_id, '$meta_key', '$meta_value'";
+			}
+
+			$import_query .= implode( " UNION ALL ", $sql_query_sel );
+			$wpdb->query( $import_query );
+		}
+
+	}
+
+
+	/**
+	 * @deprecated as of version 4.4 in WP Core
+	 * @see        get_term_meta()
+	 */
 	public function get_term_meta( $term_id, $taxonomy, $key, $single = false ){
-		return get_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $key, $single );
+		if( function_exists( 'get_term_meta' ) ){
+			return get_term_meta( $term_id, $key, $single );
+		} else {
+			return get_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $key, $single );
+		}
 	}
 
 
+	/**
+	 * @deprecated as of version 4.4 in WP Core
+	 * @see        add_term_meta()
+	 */
 	public function add_term_meta( $term_id, $taxonomy, $meta_key, $meta_value, $unique = false ){
-		return add_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $meta_key, $meta_value, $unique );
+		if( function_exists( 'add_term_meta' ) ){
+			return add_term_meta( $term_id, $meta_key, $meta_key, $unique );
+
+		} else {
+			return add_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $meta_key, $meta_value, $unique );
+		}
 	}
 
 
+	/**
+	 * @deprecated as of version 4.4 in WP Core
+	 * @see        update_term_meta()
+	 */
 	public function update_term_meta( $term_id, $taxonomy, $meta_key, $meta_value, $prev_value = '' ){
-		return update_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $meta_key, $meta_value, $prev_value );
+		if( function_exists( 'update_term_meta' ) ){
+			return update_term_meta( $term_id, $meta_key, $meta_key, $prev_value );
+
+		} else {
+			return update_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $meta_key, $meta_value, $prev_value );
+		}
 	}
 
 
+	/**
+	 * @deprecated as of version 4.4 in WP Core
+	 * @see        delete_term_meta()
+	 */
 	public function delete_term_meta( $term_id, $taxonomy, $meta_key, $meta_value = '' ){
-		return delete_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $meta_key, $meta_value );
+		if( function_exists( 'get_term_meta' ) ){
+			return delete_term_meta( $term_id, $meta_key, $meta_value );
+
+		} else {
+			return delete_metadata( 'term_taxonomy', $this->get_term_taxonomy_id( $term_id, $taxonomy ), $meta_key, $meta_value );
+		}
 	}
 
 
+	/**
+	 * @deprecated as of version 4.4 in WP Core
+	 */
 	public function get_term_taxonomy_id( $term_id, $taxonomy ){
 		if( !$term_id ){
 			return 0;
@@ -61,12 +139,7 @@ class Taxonomy_Meta {
 
 
 	/**
-	 * When a term is deleted, we need to delete all of its
-	 * associated meta
-	 *
-	 * @param int    $term_id
-	 * @param int    $tt_id
-	 * @param string $taxonomy
+	 * @deprecated as of version 4.4 in WP Core
 	 */
 	public function delete_all_term_meta( $term_id, $tt_id, $taxonomy ){
 		global $wpdb;
@@ -78,7 +151,7 @@ class Taxonomy_Meta {
 
 
 	/**
-	 * Make sure db tables exist
+	 * @deprecated as of version 4.4 in WP Core
 	 */
 	public function configure_term_meta(){
 		global $wpdb;
@@ -98,14 +171,7 @@ class Taxonomy_Meta {
 
 
 	/**
-	 * Some data from old terms seems to be stuck in the
-	 * database, and is causing corrupt terms. Delete
-	 * any taxonomy meta for the term_taxonomy_id that
-	 * already exists when the term is created.
-	 *
-	 * @param int    $term_id
-	 * @param int    $tt_id
-	 * @param string $taxonomy
+	 * @deprecated as of version 4.4 in WP Core
 	 */
 	public function delete_extra_meta( $term_id, $tt_id, $taxonomy ){
 		global $wpdb;
@@ -116,17 +182,14 @@ class Taxonomy_Meta {
 	public function drop_table( $tables ){
 		global $wpdb;
 		// don't use $wpdb->term_taxonomymeta, points to wrong blog
-		$tables[ ] = $wpdb->prefix . 'term_taxonomymeta';
+		$tables[] = $wpdb->prefix . 'term_taxonomymeta';
 
 		return $tables;
 	}
 
 
 	/**
-	 * Update the table pointer to correct blog
-	 *
-	 * @param int $new_blog_id
-	 * @param int $old_blog_id
+	 * @deprecated as of version 4.4 in WP Core
 	 */
 	public function switch_to_blog( $new_blog_id, $old_blog_id ){
 		global $wpdb;
@@ -136,7 +199,7 @@ class Taxonomy_Meta {
 
 
 	/**
-	 * Create the term meta table
+	 * @deprecated
 	 */
 	private function _create_term_taxonomy_meta_table(){
 
