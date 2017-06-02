41b03254e321bce7fd63418950b65def95c1dbd5
lipemat <mat@matlipe.com>
2017-06-02 09:09:18 -0400

    Remove $current from Cpt.php and deprecate the magic method

--- MVC/Traits/Cpt.php
+++ MVC/Traits/Cpt.php
@@ -2,8 +2,10 @@
 
 namespace MVC\Traits;
 
+use MVC\Custom_Post_Type;
+
 /**
- * Cpt
+ * Custom Post Type
  *
  * @author  Mat Lipe
  * @since   7/14/2015
@@ -21,22 +23,30 @@ trait Cpt {
 	 */
 	private $post;
 
-	/**
-	 * @var self
-	 */
-	static $current;
+	//$current no longer exists due to memory considerations
+	//You must use a separate global key
+
 	
 	public function __construct( $id ){
 		$this->post_id = $id;
-		self::$current = $this;
 	}
 
 
 	public function __get( $name ){
+		_deprecated_function( '__get', '2.4.1', '$this->get_post()->{key}');
 		return $this->{$name} = $this->get_post()->{$name};
 	}
 
 
+	public function get_id(){
+		return $this->post_id;
+	}
+
+	/**
+	 * Get the WP post from current context
+	 *
+	 * @return null|\WP_Post
+	 */
 	public function get_post(){
 		if( empty( $this->post ) ){
 			$this->post = get_post( $this->post_id );
@@ -63,7 +73,20 @@ trait Cpt {
 	 * @return void
 	 */
 	public static function register_post_type(){
-		self::$cpt = new \MVC\Custom_Post_Type( self::POST_TYPE );
+		self::$cpt = new Custom_Post_Type( self::POST_TYPE );
+	}
+
+
+	/**
+	 *
+	 * @param int $post_id
+	 *
+	 * @static
+	 *
+	 * @return self
+	 */
+	public static function factory( $post_id ){
+		return new self( $post_id );
 	}
 
 
